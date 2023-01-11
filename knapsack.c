9f64454e3261184430311cdab5ca96fa7e5aa4ab
milo <milo@ubuntu-22.04.1-lts.myguest.virtualbox.org>
2023-01-12 00:16:00 +0100

    blocks WIP

--- knapsack.c
+++ knapsack.c
@@ -42,11 +42,42 @@ int knapsack_bottom_up_parallel(int capacity, int weights[], int values[], int n
 	return table[num_items][capacity];
 }
 
+int knapsack_bottom_up_parallel_blocks(int capacity, int weights[], int values[], int num_items)
+{
+	int i, w, block;
+	int table[num_items+1][capacity+1];
+
+	int num_blocks = omp_get_max_threads();
+	printf("%d\n", num_blocks);
+
+	#pragma omp parallel private(block, i, w)
+	{
+		#pragma omp for
+		for (block = 0; block < num_blocks; block++) {
+			printf("xd\n");
+			for (i = 0; i <= num_items; i++) {
+				for (w = block * capacity/num_blocks; w < block+1 * capacity/num_blocks, w <= capacity; w++) {
+					if (i == 0 || w == 0)
+						table[i][w] = 0;
+					else if (weights[i-1] <= w)
+						table[i][w] = max(table[i-1][w], table[i-1][w-weights[i-1]] + values[i-1]);
+					else
+						table[i][w] = table[i-1][w];
+				}
+
+				#pragma omp barrier
+			}
+		}
+	}
+
+	return table[num_items][capacity];
+}
+
 int main()
 {
-    int weights[] = {1, 3, 4, 5};
-    int values[] = {1, 4, 5, 7};
-	int capacity = 9;
+    int weights[] = {1, 3, 4, 5, 6, 2, 7, 5, 4, 9, 1, 2, 3};
+    int values[] = {1, 4, 5, 7, 8, 1, 9, 4, 4, 10, 2, 3, 4};
+	int capacity = 14;
 	int num_items = sizeof(values) / sizeof(values[0]);
 
     double wtime = omp_get_wtime();
@@ -57,10 +88,16 @@ int main()
     wtime = omp_get_wtime();
     int parallel_val = knapsack_bottom_up_parallel(capacity, weights, values, num_items);
     wtime = omp_get_wtime() - wtime;
-    printf("Parallel algorithm took %f seconds\n", wtime);
+    printf("Parallel algorithm (inner loop) took %f seconds\n", wtime);
+
+	wtime = omp_get_wtime();
+    int parallel_blocks_val = knapsack_bottom_up_parallel_blocks(capacity, weights, values, num_items);
+    wtime = omp_get_wtime() - wtime;
+    printf("Parallel algorithm (blocks) took %f seconds\n", wtime);
 
 	printf("Maximum value %d in knapsack with capacity %d\n", seq_val, capacity);
     printf("Maximum value %d in knapsack with capacity %d\n", parallel_val, capacity);
+	printf("Maximum value %d in knapsack with capacity %d\n", parallel_blocks_val, capacity);
 
 	return 0;
 }
